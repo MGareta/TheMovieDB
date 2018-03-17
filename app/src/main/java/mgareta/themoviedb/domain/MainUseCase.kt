22f742428d107ff6cf22cda5d29609c0b43e40a5
Marc Gareta Manzano <marc.gareta@outlook.com>
2018-03-17 11:15:17 +0100

    Main domain
    Use case main activity

---
+++ app/src/main/java/mgareta/themoviedb/domain/MainUseCase.kt
@@ -0,0 +1,14 @@
+package mgareta.themoviedb.domain
+
+import io.reactivex.Observable
+import mgareta.themoviedb.data.rest.model.ResultMovie
+
+/**
+ * Created by marc on 16/03/18.
+ */
+
+interface MainUseCase {
+
+    fun getPopularMovie(page: Int): Observable<List<ResultMovie>>
+
+}
